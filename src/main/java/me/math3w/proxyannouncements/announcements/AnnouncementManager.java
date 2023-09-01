4fe0b0306b76d346af7a61782a72333c88b0d048
Math3w <math3wmail@gmail.com>
2023-09-01 16:02:14 +0200

    feat: message for forcing announcement

--- src/main/java/me/math3w/proxyannouncements/announcements/AnnouncementManager.java
+++ src/main/java/me/math3w/proxyannouncements/announcements/AnnouncementManager.java
@@ -1,12 +1,14 @@
 package me.math3w.proxyannouncements.announcements;
 
 import me.math3w.proxyannouncements.ProxyAnnouncements;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.config.Configuration;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class AnnouncementManager {
     private final ProxyAnnouncements proxyAnnouncements;
@@ -14,6 +16,7 @@ public class AnnouncementManager {
     private final int interval;
     private final boolean random;
     private final Set<String> servers;
+    private int currentAnnouncement = 0;
 
     public AnnouncementManager(ProxyAnnouncements proxyAnnouncements, List<Announcement> announcements, int interval, boolean random, Set<String> servers) {
         this.proxyAnnouncements = proxyAnnouncements;
@@ -21,6 +24,17 @@ public class AnnouncementManager {
         this.interval = interval;
         this.random = random;
         this.servers = servers.contains("*") ? proxyAnnouncements.getProxy().getServers().keySet() : servers;
+
+        proxyAnnouncements.getProxy().getScheduler().schedule(proxyAnnouncements, () -> {
+            Announcement announcement = announcements.get(currentAnnouncement);
+
+            sendAnnouncement(announcement);
+
+            currentAnnouncement++;
+            if (currentAnnouncement >= announcements.size()) {
+                currentAnnouncement = 0;
+            }
+        }, interval, interval, TimeUnit.SECONDS);
     }
 
     public AnnouncementManager(ProxyAnnouncements proxyAnnouncements, Configuration configuration, Configuration announcementsConfiguration) {
@@ -31,6 +45,14 @@ public class AnnouncementManager {
                 configuration.getInt("interval"),
                 configuration.getBoolean("random"),
                 new HashSet<>(configuration.getStringList("servers")));
+    }
+
+    public void sendAnnouncement(Announcement announcement) {
+        servers.stream().map(serverName -> proxyAnnouncements.getProxy().getServerInfo(serverName)).forEach(server -> {
+            for (ProxiedPlayer player : server.getPlayers()) {
+                announcement.sendAnnouncement(player);
+            }
+        });
     }
 
     public Optional<Announcement> getAnnouncement(String name) {
