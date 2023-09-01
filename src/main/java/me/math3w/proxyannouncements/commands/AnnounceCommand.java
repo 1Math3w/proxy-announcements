d0c9fe114058f87a3021efa532844dca537070d2
Math3w <math3wmail@gmail.com>
2023-09-01 16:01:50 +0200

    feat: announcement scheduler

--- src/main/java/me/math3w/proxyannouncements/commands/AnnounceCommand.java
+++ src/main/java/me/math3w/proxyannouncements/commands/AnnounceCommand.java
@@ -1,11 +1,10 @@
 package me.math3w.proxyannouncements.commands;
 
 import me.math3w.proxyannouncements.ProxyAnnouncements;
+import me.math3w.proxyannouncements.announcements.AnnouncementManager;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.config.ServerInfo;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class AnnounceCommand extends Command {
@@ -26,12 +25,11 @@ public class AnnounceCommand extends Command {
         }
 
         String announcementName = args[0];
-        proxyAnnouncements.getAnnouncementManager().getAnnouncement(announcementName).ifPresentOrElse(announcement -> {
-            for (ServerInfo server : proxyAnnouncements.getProxy().getServers().values()) {
-                for (ProxiedPlayer player : server.getPlayers()) {
-                    announcement.sendAnnouncement(player);
-                }
-            }
+        AnnouncementManager announcementManager = proxyAnnouncements.getAnnouncementManager();
+        announcementManager.getAnnouncement(announcementName).ifPresentOrElse(announcement -> {
+            announcementManager.sendAnnouncement(announcement);
+            sender.sendMessage(new TextComponent(ChatColor.GRAY + "Successfully announced " + ChatColor.RED + announcementName +
+                    ChatColor.GRAY + " to servers " + ChatColor.RED + String.join(", ", announcementManager.getServers())));
         }, () -> {
             TextComponent message = new TextComponent("Invalid announcement name!");
             message.setColor(ChatColor.RED);
